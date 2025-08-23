2ad2d0ced9214b55b3460d7c60c742afeee4a462
NSakib26 <n.sakib2640@gmail.com>
2025-08-23 23:26:01 +0600

    Method syntax in Java

---
+++ Method (function)/basicMethodsyntax.java
@@ -0,0 +1,13 @@
+public class basicMethodsyntax{
+    public static void HelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World ");
+        
+    }
+    public static void main(String[] args) {
+        //function call
+        HelloWorld();
+        
+    }
+}
