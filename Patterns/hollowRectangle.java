ff5a8b9b8782fcf726a193f81b22d3a648b9c4ad
NSakib26 <n.sakib2640@gmail.com>
2025-08-28 07:59:40 +0600

    Patterns folder created and print hellow rectangle

---
+++ Patterns/hollowRectangle.java
@@ -0,0 +1,29 @@
+// * * * * * 
+// *       *
+// *       *
+// * * * * *
+
+
+public class hollowRectangle{
+
+    public static void helloRect(int totRows, int totCols){
+        for(int i=1 ;i<=totRows;i++){
+            for(int j=1 ;j<=totCols;j++){
+                if(i==1||j==1|| i==totRows|| j== totCols){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+
+                
+            }
+                System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        helloRect(4,5);
+        
+    }
+}
